ede303b57e0c789c4efb4ab6fc1fb9c5f00788bc
6MA-606 <zyxma.6666@hotmail.com>
2023-07-28 01:25:00 +0700

    Add species data and evolution chain.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
-import { PokeAbility, PokePolygonStat, PokeRangeStat, PokeType } from './components/PokeInfo';
+import { PokeAbility, PokeEggGroups, PokeEvoChain, PokePolygonStat, PokeRangeStat, PokeType } from './components/PokeInfo';
 import { RevolvingDot } from 'react-loader-spinner';
 import { PokeButton, ShinyButton } from './components/Button';
 
@@ -9,7 +9,8 @@ function App() {
   const [species, setSpecies] = useState(null);
   const [loading, setLoading] = useState(false);
   const [shiny, setShiny] = useState(false);
-  const [id, setId] = useState(1);
+  const [id, setId] = useState(133);
+  const [description, setDescription] = useState('');
 
   useEffect(() => {
 
@@ -42,6 +43,13 @@ function App() {
 
   }, [id]);
 
+  useEffect(() => {
+    if (species) {
+      let flavor = species.flavor_text_entries.filter(entry => entry.language.name === "en");
+      setDescription(flavor[flavor.length - 1].flavor_text);
+    }
+  }, [species]);
+
   return (
     <div className='min-h-screen flex gap-6 justify-center items-center'>
       { id > 1 ? (
@@ -50,70 +58,83 @@ function App() {
         <div className="w-24"></div>
       )}
       <div className='flex gap-5 items-center justify-center'>
-        <div className={`h-[30rem] w-[30rem] relative border-2 bg-neutral-700 border-neutral-600 rounded-xl`}>
-          <div className='absolute w-full flex flex-col items-center z-10'>
-            <h1 className='text-[3rem] text-neutral-50 font-semibold capitalize drop-shadow-lg'>{pokemon?.name}</h1>
-            <h3 className='text-sm font-semibold text-neutral-400 drop-shadow-lg'>#{pokemon?.id.toString().padStart(3, '0')}</h3>
+        <div className='flex flex-col'>
+          <div className={`h-[30rem] w-[30rem] relative border-2 bg-neutral-700 border-neutral-600 rounded-xl`}>
+            <div className='w-full h-full absolute flex items-center justify-center'>
+              {loading ? (
+                <RevolvingDot
+                radius="70"
+                color="#ff0000"
+                secondaryColor=''
+                ariaLabel="revolving-dot-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                />
+                ) : (
+                  <>
+                    <img
+                      className='w-[90%] h-[90%] z-10'
+                      src={!shiny ? pokemon?.sprites.other.home.front_default : pokemon?.sprites.other.home.front_shiny}
+                      alt={pokemon?.name}
+                    />
+                    <div className='absolute bottom-0 w-[80%] h-[20%] bg-gradient-radial opacity-40 rounded-[100%]'></div>
+                  </>
+                )}
+            </div>
+            <ShinyButton isShiny={shiny} onClick={() => setShiny(!shiny)} className="absolute right-0 bottom-0" />
           </div>
-          <div className='w-full h-full absolute flex items-center justify-center'>
-            {loading ? (
-              <RevolvingDot
-              radius="70"
-              color="#ff0000"
-              secondaryColor=''
-              ariaLabel="revolving-dot-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={true}
-              /> 
-              ) : (
-                <>
-                  <img
-                  className='w-[90%] h-[90%] z-10'
-                  src={!shiny ? pokemon?.sprites.other.home.front_default : pokemon?.sprites.other.home.front_shiny}
-                  alt={pokemon?.name}
-                  />
-                  <div className='absolute bottom-0 w-[80%] h-[20%] bg-gradient-radial opacity-40 rounded-[100%]'></div>
-                </>
-              )}
+          <div className='h-[19rem] mt-[1rem] bg-neutral-700 border-2 border-neutral-600 rounded-xl'>
+            <div className='flex flex-col px-5 h-full gap-5'>
+              <div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-baseline justify-center gap-2">
+                    <div className='text-[3rem] text-neutral-50 font-semibold capitalize drop-shadow-lg'>{pokemon?.name}</div>
+                    <div className='text-[2rem] font-semibold text-neutral-400 drop-shadow-lg'>#{pokemon?.id.toString().padStart(3, '0')}</div>
+                  </div>
+                  <div>
+                    <div>
+                      <span className='text-xs text-neutral-400 font-semibold'>Height: </span>
+                      <span className='text-xs text-neutral-300'>{pokemon?.height / 10} m</span>
+                    </div>
+                    <div>
+                      <span className='text-xs text-neutral-400 font-semibold'>Weight: </span>
+                      <span className='text-xs text-neutral-300'>{pokemon?.weight / 10} kg</span>
+                    </div>
+                  </div>
+                </div>
+                <div className='flex gap-2 items-center mt-1 mb-2'>
+                  {pokemon?.types.map((type, index) => (
+                    <PokeType key={index} poketype={type.type.name} />
+                  ))}
+                </div>
+                <div className='bg-neutral-700 text-sm text-neutral-300 font-semibold ml-1 capitalize'>{species?.genera[7].genus}</div>
+              </div>
+              <fieldset className='min-h-[6.25rem] border-[1px] border-neutral-500 rounded-lg'>
+                <legend className='text-sm text-neutral-400 font-semibold px-1 ml-1'>Description</legend>
+                <div className='text-sm text-neutral-300 font-semibold text-center whitespace-pre-wrap mb-4'>{description}</div>
+              </fieldset>
+            </div>
           </div>
-          <ShinyButton isShiny={shiny} onClick={() => setShiny(!shiny)} className="absolute right-0 bottom-0" />
         </div>
-        <div className='h-[30rem] w-[50vw] flex flex-col border-2 bg-neutral-700 border-neutral-600 rounded-xl overflow-hidden'>
+        <div className='h-[50rem] w-[60rem] flex flex-col border-2 bg-neutral-700 border-neutral-600 rounded-xl overflow-hidden'>
           <div className='px-3 py-1 bg-red-700 flex items-center gap-2'>
             <div className='text-[1.5rem] text-neutral-50 capitalize'>
               {pokemon?.name}
               <span className='text-sm font-semibold text-neutral-400'> #{pokemon?.id.toString().padStart(3, '0')}</span>
             </div>
-            <div className='bg-neutral-700 px-3 py-1 rounded-md italic text-sm text-neutral-300 font-semibold ml-1 capitalize'>{species?.genera[7].genus}</div>
           </div>
-          <div className='p-3'>
+          <div className='p-3 flex flex-col gap-3 overflow-y-auto'>
             <div className='flex gap-6'>
               <div className='flex-1'>
-                <div className='flex gap-1 items-center'>
-                  <span className='text-sm text-neutral-400 font-semibold'>Types:</span>
-                  {pokemon?.types.map((type, index) => (
-                    <PokeType key={index} poketype={type.type.name} />
-                  ))}
-                </div>
                 <PokeAbility abilities={pokemon?.abilities} />
               </div>
               <div className='flex-1'>
-                <div className='text-sm text-neutral-400 font-semibold'>Other Info</div>
-                <div className='ml-2'>
-                  <div>
-                    <span className='text-xs text-neutral-400 font-semibold'>Height: </span>
-                    <span className='text-xs text-neutral-300'>{pokemon?.height / 10} m</span>
-                  </div>
-                  <div>
-                    <span className='text-xs text-neutral-400 font-semibold'>Weight: </span>
-                    <span className='text-xs text-neutral-300'>{pokemon?.weight / 10} kg</span>
-                  </div>
-                </div>
+                <PokeEggGroups eggGroups={species?.egg_groups} />
               </div>
             </div>
             <fieldset className='h-72 border-[1px] border-neutral-500 rounded-lg flex justify-evenly items-center'>
-              <legend className='text-sm text-neutral-400 font-semibold px-1 ml-1 ab'>Base Stats</legend>
+              <legend className='text-sm text-neutral-400 font-semibold px-1 ml-1'>Base Stats</legend>
               <PokePolygonStat
                 pokeType={pokemon?.types[0].type.name}
                 stat={{
@@ -137,6 +158,7 @@ function App() {
                 }}
               />
             </fieldset>
+            <PokeEvoChain species={species} />
           </div>
         </div>
       </div>
--- src/components/PokeInfo.jsx
+++ src/components/PokeInfo.jsx
@@ -1,6 +1,9 @@
+import axios from "axios";
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts";
+import { ArrowLeft, ArrowRight } from "react-bootstrap-icons";
+import { RevolvingDot } from "react-loader-spinner";
 
 const dataColors = (type) => {
   switch (type) {
@@ -46,17 +49,19 @@ const dataColors = (type) => {
 };
 
 export const PokeType = (props) => {
-  const { poketype } = props;
+  const { poketype, size } = props;
 
   return (
-    <div
-      className={`bg-type-${poketype} py-0.5 rounded w-20 uppercase font-semibold text-sm text-white flex items-center justify-center`}
-    >
-      <div>
-        <div className="translate-x-[0.09rem] translate-y-[0.09rem] text-black opacity-25">
-          {poketype}
+    <div style={{ fontSize: (size || "1rem")}}>
+      <div
+        className={`bg-type-${poketype} rounded w-[6em] h-[1.75em] uppercase font-semibold text-[0.875em] text-white flex items-center justify-center`}
+      >
+        <div className="relative">
+          <div className="translate-x-[0.09rem] translate-y-[0.09rem] text-black opacity-25">
+            {poketype}
+          </div>
+          <div className="absolute -translate-y-5 text-white">{poketype}</div>
         </div>
-        <div className="absolute -translate-y-5 text-white">{poketype}</div>
       </div>
     </div>
   );
@@ -64,6 +69,7 @@ export const PokeType = (props) => {
 
 PokeType.propTypes = {
   poketype: PropTypes.string.isRequired,
+  size: PropTypes.string,
 };
 
 export const PokePolygonStat = (props) => {
@@ -221,8 +227,8 @@ export const PokeAbility = (props) => {
   const { abilities } = props;
 
   return (
-    <div className="my-2">
-      <span className="text-sm text-neutral-400 font-semibold">Abilities:</span>
+    <div className="">
+      <span className="text-sm text-neutral-400 font-semibold">Abilities</span>
       <div className="capitalize flex gap-2">
         {abilities?.map((ability, index) => (
           <div
@@ -246,4 +252,284 @@ export const PokeAbility = (props) => {
 
 PokeAbility.propTypes = {
   abilities: PropTypes.array,
+};
+
+export const PokeEggGroups = (props) => {
+
+  const { eggGroups } = props;
+
+  return (
+    <div className="">
+      <span className="text-sm text-neutral-400 font-semibold">Egg Groups</span>
+      <div className="capitalize flex gap-2 my-2">
+        {eggGroups?.map((eggGroup, index) => (
+          <div key={index} className={`bg-egg-${eggGroup.name} rounded w-20 font-semibold text-sm text-white flex items-center justify-center`}>
+            <div className="relative">
+              <div className="translate-x-[0.09rem] translate-y-[0.09rem] text-black opacity-25">
+                {eggGroup.name}
+              </div>
+              <div className="absolute -translate-y-5 text-white">{eggGroup.name}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+PokeEggGroups.propTypes = {
+  eggGroups: PropTypes.array,
+};
+
+export const PokeEvoChain = (props) => {
+
+  const { species } = props;
+
+  const [loading, setLoading] = useState(false);
+  const [evolutionChain, setEvolutionChain] = useState(null);
+  
+  useEffect(() => {
+    
+    let abortController = new AbortController();
+
+    const fetchEvoChain = async () => {
+      setLoading(true);
+      try {
+        await axios.get(species?.evolution_chain?.url , {
+          signal: abortController.signal
+        }).then((res) => {
+          setEvolutionChain(res.data)
+          console.log(res.data)
+        });
+      } catch (error) {
+        if (!axios.isCancel(error)) {
+          console.error("Something went wrong, ", error);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEvoChain();
+
+    return () => {
+      abortController.abort();
+    };
+  }, [species]);
+
+  return (
+    <div className="">
+      {evolutionChain?.chain?.evolves_to[0] === undefined ? null : (
+        <>
+          <span className="text-sm text-neutral-400 font-semibold">Evolution Chain</span>
+          <div className="flex justify-center gap-2 my-2">
+            {loading ? (
+              <div className="text-sm text-neutral-300 font-semibold">Loading...</div>
+            ) : (
+              <div className="text-sm text-neutral-300 font-semibold flex gap-2">
+                <PokeEvoDiagram species={species} evoChain={evolutionChain} />
+              </div>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+PokeEvoChain.propTypes = {
+  species: PropTypes.object,
+};
+
+const PokeEvoDiagram = (props) => {
+
+  const { species, evoChain } = props;
+
+  const evoTrigger = (evoDetail, name) => {
+
+    let triggers = [];
+
+    if (evoDetail?.trigger?.name === "level-up") {
+      if (evoDetail?.min_happiness !== null) {
+        triggers.push(`High friendship`);
+      }
+      if (evoDetail?.min_level !== null) {
+        triggers.push(`Level ${evoDetail?.min_level}`);
+      }
+      if (evoDetail?.min_beauty !== null) {
+        triggers.push(`High beauty`);
+      }
+      if (evoDetail?.min_affection !== null) {
+        triggers.push(`High affection`);
+      }
+    }
+
+    if (evoDetail?.trigger?.name === "use-item") {
+      triggers.push(`Use ${evoDetail?.item?.name}`);
+
+      let gender = evoDetail?.gender;
+      if (gender !== null) {
+        triggers.push(`Gender ${gender === 1 ? "Female" : "Male"}`);
+      }
+    }
+
+    if (evoDetail?.trigger?.name === "trade") {
+      triggers.push(`Trade`);
+    }
+
+    if (evoDetail?.trigger?.name === "shed") {
+      triggers.push(`Shed`);
+    }
+
+    if (evoDetail?.trigger?.name === "other") {
+      triggers.push(`Other`);
+    }
+
+    switch (evoDetail?.time_of_day) {
+      case "day":
+        triggers.push(` in day time`);
+        break;
+      case "night":
+        triggers.push(` in night time`);
+        break;
+      default:
+        break;
+    }
+
+    if (name === "leafeon" || name === "glaceon") {
+      triggers.push(`Use ${name === "leafeon" ? "Leaf\u00A0stone" : "Ice\u00A0stone"}`);
+      triggers.push(`or level up near an ${name === "leafeon" ? "Moss\u00A0Rock" : "Ice\u00A0Rock"}`);
+    }
+
+    if (name === "sylveon") {
+      triggers = [];
+      triggers.push(`Level up with high friendship`);
+      triggers.push(`and know a Fairy-type move`);
+    }
+
+    return (
+      <div className="text-[0.6rem] text-neutral-400 text-center leading-4">
+        {triggers.map((trigger, index) => (
+          <div key={index}>{trigger}</div>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex items-center">
+      <div className="flex flex-col items-center gap-5">
+        {evoChain?.chain?.species?.name === "eevee" ? (
+          evoChain?.chain?.evolves_to?.map((evo, index) => (
+            <PokeCardMini key={index} pokeId={(evoChain?.chain?.species?.url)?.split("/")[6]} pokeName={evoChain?.chain?.species?.name} />
+          ))
+        ) : (
+          <PokeCardMini pokeId={(evoChain?.chain?.species?.url)?.split("/")[6]} pokeName={evoChain?.chain?.species?.name} />
+        )}
+      </div>
+
+      <div className="flex flex-col gap-5">
+        {evoChain?.chain?.evolves_to?.length > 0 ? (
+          evoChain?.chain?.evolves_to?.map((evo, index) => (
+            <div key={index} className="flex flex-col items-center gap-2">
+              <div className="flex items-center justify-center">
+                <div className="flex">
+                  <div className="w-[7rem] h-20 flex flex-col justify-center items-center">
+                    <div>{evoTrigger(evo?.evolution_details[0], evo?.species?.name)}</div>
+                    <ArrowRight className="text-neutral-400 text-xl" />
+                  </div>
+                  <PokeCardMini pokeId={evo?.species?.url.split("/")[6]} pokeName={evo?.species?.name} />
+                </div>
+                <div className="flex flex-col gap-5">
+                  {evo?.evolves_to?.length > 0 ? (
+                    evo?.evolves_to?.map((evo, index) => (
+                      <div key={index} className="flex">
+                        <div className="w-[7rem] h-20 flex flex-col justify-center items-center">
+                          <div>{evoTrigger(evo?.evolution_details[0])}</div>
+                          <ArrowRight className="text-neutral-400 text-xl" />
+                        </div>
+                        <PokeCardMini pokeId={evo?.species?.url.split("/")[6]} pokeName={evo?.species?.name} />
+                      </div>
+                    ))
+                  ) : null}
+                </div>
+              </div>
+            </div>
+          ))
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+PokeEvoDiagram.propTypes = {
+  species: PropTypes.object,
+  evoChain: PropTypes.object,
+};
+
+export const PokeCardMini = (props) => {
+
+  const { pokeId, pokeName } = props;
+
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+ 
+    let abortController = new AbortController();
+
+    const fetchPokeType = async () => {
+      try {
+        setLoading(true);
+        await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}`, {
+          signal: abortController.signal
+        }).then((res) => {
+          setPokemon(res.data);
+        });
+      } catch (error) {
+        if (!axios.isCancel(error)) console.error("Something went wrong, ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPokeType();
+
+    return () => {
+      abortController.abort();
+    }
+
+  }, [pokeId]);
+
+  return (
+    <div className="flex flex-col items-center gap-1 w-20">
+      <div className="bg-neutral-800 w-20 h-20 flex flex-col justify-center items-center rounded-lg">
+        {!pokemon?.sprites?.front_default ? (
+          <div className="w-20 h-20 flex justify-center items-center">
+            <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-neutral-300"></div>
+          </div>
+        ) : (
+          <img
+            className="w-20 h-20"
+            src={pokemon?.sprites?.front_default}
+            alt={pokeName}
+          />
+        )}
+      </div>
+      <div className="flex items-baseline gap-1">
+        <div className="text-sm text-neutral-300 font-semibold capitalize whitespace-nowrap">{pokeName}</div>
+        <div className="text-xs text-neutral-400">#{pokeId?.toString().padStart(3, '0')}</div>
+      </div>
+      <div className="flex gap-1 h-4">
+        {pokemon?.types?.map((type, index) => (
+          <PokeType key={index} poketype={type.type.name} size={"0.6rem"} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+PokeCardMini.propTypes = {
+  pokeId: PropTypes.string,
+  pokeName: PropTypes.string,
 };
